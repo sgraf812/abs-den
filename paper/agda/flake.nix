ac4456d7b500f11212261762cf5ee25161ec0c58
Sebastian Graf <sebastian.graf@kit.edu>
2023-06-16 11:33:06 +0200

    Some Flakifikation and direnv

---
+++ paper/agda/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.agda.withPackages (p: [ p.standard-library ]))
+          ];
+        };
+      });
+}
+
