3eb644d40ad90d9dfa58449289153c813bb778cd
Sebastian Graf <sebastian.graf@kit.edu>
2023-10-10 14:22:16 +0200

    Finally a working nix build

--- paper/flake.nix
+++ paper/flake.nix
@@ -1,21 +1,17 @@
 {
-  description = "Build env";
+  description = "Build the document";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
-    lhs2tex-src.url = "github:sgraf812/lhs2TeX/fix-with-srcdist";
-    lhs2tex-src.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, lhs2tex-src }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
         texlive = pkgs.texlive.combined.scheme-full;
-
-        lhs2tex = pkgs.haskellPackages.lhs2tex.overrideAttrs (_: { src = lhs2tex-src + "/lhs2tex-1.24.tar.gz"; });
       in {
         packages.abs-den = pkgs.stdenvNoCC.mkDerivation {
           name = "abs-den";
@@ -26,7 +22,13 @@
             texlive
           ];
           builder = "${pkgs.bash}/bin/bash";
-          args = [ "-c" "source $stdenv/setup && cp -rL --no-preserve=all $src paper/ && cd paper/ && lhs2TeX -v --poly abs-den.lhs -o /dev/null && ghci -pgmL lhs2TeX -optL--pre -XPartialTypeSignatures -v0 -ignore-dot-ghci ./interp.lhs && lhs2TeX --pre -o interp.tex ./abs-den.lhs && echo here && ${pkgs.lhs2tex}/bin/lhs2TeX -v --poly -o abs-den.hs abs-den.lhs && make abs-den.pdf && cp abs-den.pdf $out" ];
+          args = [ "-c" ''
+            source $stdenv/setup
+            mkdir -p $out
+            cp -r $src/* .
+            make abs-den.pdf abs-den-ext.pdf
+            cp *.pdf $out/
+          ''];
           src = ./.;
           system = "x86_64-linux";
         };
@@ -34,11 +36,7 @@
         defaultPackage = self.packages.${system}.abs-den;
 
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            ghc
-            lhs2tex
-            texlive
-          ];
+          inputsFrom = builtins.attrValues self.packages.${system};
         };
       });
 }
