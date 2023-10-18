1cb5911f61ad8aa5006dcc45c8cedaace55c8bdd
Sebastian Graf <sgraf1337@gmail.com>
2023-10-18 09:17:02 +0200

    Working on a Hedgehog generator

---
+++ paper/hs/Gen.hs
@@ -0,0 +1,137 @@
+module Gen where
+
+import           Hedgehog hiding (Var)
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import           Data.Set (Set)
+import qualified Data.Set as Set
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Char
+import Text.Show
+import Control.Monad
+import Control.Monad.Trans.State
+import Abstractions hiding ((+))
+
+import Expr hiding (Env)
+import GHC.Stack
+
+newtype Env = Env { nextFree :: Int }
+
+instance Show Env where
+  showsPrec _ = showListWith showString . boundVars
+
+idx2Name :: Int -> Name
+idx2Name n | n <= 26   = [chr (ord 'a' + n)]
+           | otherwise = "t" ++ show n
+
+boundVars :: Env -> [Name]
+boundVars (Env n) = map idx2Name [0..n-1]
+
+mkEnvWithNVars :: Int -> Env
+mkEnvWithNVars = Env
+
+emptyEnv :: Env
+emptyEnv = mkEnvWithNVars 0
+
+env :: Gen Env
+env = Gen.sized $ \size -> Gen.element (map mkEnvWithNVars [0..max 0 (unSize size)])
+
+closedExpr :: Gen Expr
+closedExpr = openExpr emptyEnv
+
+openExpr :: Env -> Gen Expr
+openExpr env = uniqify <$> openExprShadow env
+  where
+    uniqify e = evalState (go Map.empty e) emptyEnv
+    go benv (Let n e1 e2) = do
+      n' <- fresh
+      let benv' = Map.insert n n' benv
+      Let n' <$> go benv' e1 <*> go benv' e2
+    go benv (Lam n e) = do
+      n' <- fresh
+      let benv' = Map.insert n n' benv
+      Lam n' <$> go benv' e
+    go benv (Var n) =
+      pure (Var (Map.findWithDefault n n benv))
+    go benv (App e n) =
+      App <$> go benv e <*> pure (Map.findWithDefault n n benv)
+    go benv (ConApp k ns) =
+      pure (ConApp k (map (\n -> Map.findWithDefault n n benv) ns))
+    go benv (Case e alts) =
+      Case <$> go benv e <*> traverse alt alts
+        where
+          alt (k,ns,rhs) = do
+            ns' <- traverse (const fresh) ns
+            let benv' = foldr (uncurry Map.insert) benv (zip ns ns')
+            rhs' <- go benv' rhs
+            pure (k,ns',rhs')
+    fresh = state $ \env -> (idx2Name $ nextFree env, env{nextFree = nextFree env + 1})
+
+-- | May cause shadowing. Will be cleaned up in openExpr
+openExprShadow :: Env -> Gen Expr
+-- TODO: Try Gen.recursive
+openExprShadow env = Gen.sized $ \size ->
+  Gen.frequency $ concat
+    [ [ ((unSize size `div` 10) + 2, boundName env) | not $ null $ boundVars env ]
+    , [ ((unSize size `div` 3) + 1, let_ env) ]
+    , [ ((unSize size `div` 3) + 1, lam env)      ]
+    , [ ((unSize size `div` 3) + 1, app env)  | not $ null $ boundVars env ]
+    , [ ((unSize size `div` 3) + 1, conApp env)  | not $ null $ boundVars env ]
+    , [ ((unSize size `div` 3) + 1, case_ env)  | not $ null $ boundVars env ]
+    ]
+
+myElement :: HasCallStack => [a] -> Gen a
+myElement = Gen.element
+
+boundName, app, lam, let_, conApp, case_ :: Env -> Gen Expr
+boundName env = Gen.element (map Var (boundVars env))
+app       env =
+  App <$> Gen.scale (max 0 . subtract 1) (openExprShadow env)
+      <*> Gen.element (boundVars env)
+lam       env = withBoundName env $ \v env' ->
+  Lam v <$> Gen.scale (max 0 . subtract 1) (openExprShadow env')
+let_      env = withBoundName env $ \v env' ->
+  Let v <$> Gen.small (openExprShadow env')
+        <*> Gen.small (openExprShadow env')
+conApp    env = do
+  k :: Tag <- Gen.enumBounded
+  ConApp k <$> replicateM (conArity k) (Gen.element (boundVars env))
+case_     env = do
+  tc :: TyCon <- Gen.enumBounded
+  let ks = tyConTags tc
+  let alt k = withNBoundNames (conArity k) env $ \vs env' -> do
+        rhs <- Gen.small (openExprShadow env)
+        pure (k,vs,rhs)
+  Case <$> Gen.scale (max 0 . subtract 1) (openExprShadow env)
+       <*> traverse alt (tyConTags tc)
+
+withBoundName :: Env -> (Name -> Env -> Gen a) -> Gen a
+withBoundName env f = fresh -- dont want shadowing : [ shadowing | not $ null $ boundVars env ]
+  where
+    fresh = do
+      let tv   = idx2Name (nextFree env)
+          env' = env { nextFree = nextFree env + 1 }
+      f tv env'
+    shadowing = do
+      tv <- Gen.element (boundVars env)
+      f tv env
+
+withNBoundNames :: Int -> Env -> ([Name] -> Env -> Gen a) -> Gen a
+withNBoundNames n env f = go n [] env
+  where
+    go 0 vs env = f vs env
+    go n vs env = withBoundName env $ \v env' -> go (n-1) (v:vs) env'
+
+--exprDepth :: Expr -> Int
+--exprDepth (Fix (Var _)) = 0
+--exprDepth (Fix (App f a)) = 1 + exprDepth f
+--exprDepth (Fix (Lam _ e)) = 1 + exprDepth e
+--exprDepth (Fix (Let _ e1 e2)) = 1 + max (exprDepth e1) (exprDepth e2)
+
+--exprSize :: Expr -> Int
+--exprSize (Fix (Var _)) = 1
+--exprSize (Fix (App f a)) = 1 + exprSize f
+--exprSize (Fix (Lam _ e)) = 1 + exprSize e
+--exprSize (Fix (Let _ e1 e2)) = 1 + exprSize e1 + exprSize e2
+
---
+++ paper/hs/TestMain.hs
@@ -0,0 +1,188 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module TestMain (main) where
+
+import           Hedgehog hiding (label)
+import           Hedgehog.Gen (sized, int)
+import           Hedgehog.Range (constant)
+import           Control.Monad
+import qualified Gen
+import           Control.Monad.IO.Class
+import           Debug.Trace
+import qualified Data.Map as Map
+import           Data.Maybe
+import           Data.String
+import qualified Data.List.NonEmpty as NE
+import           Text.Printf
+import           Control.Concurrent
+import           Control.Concurrent.Async.Lifted (race)
+
+import           Expr hiding (assert)
+import           Stateless
+import qualified Cont
+import qualified Stateful
+import qualified DynamicEnv
+import qualified Stackless
+import qualified Envless
+import qualified LessToFull
+
+
+main :: IO ()
+main = void $
+  checkParallel $$(discover)
+
+sizeFactor :: Int
+sizeFactor = 2
+
+withTimeLimit :: Int -> TestT IO a -> TestT IO a
+withTimeLimit timeout v = do
+  result <-
+    race
+      (liftIO $ threadDelay timeout)
+      v
+  case result of
+    Left () -> fail "Timeout exceeded"
+    Right x -> pure x
+
+prop_Eq_Expr =
+  property $ do
+    --e <- forAll (sized (\s -> trace (show s) Gen.closedExpr))
+    --liftIO $ print (Gen.exprSize e)
+    --liftIO $ print (Gen.exprDepth e)
+    --liftIO $ print e
+    e <- forAll Gen.closedExpr
+    e === e
+
+prop_maxinf_maximal_trace_stuck_or_balanced =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    let p = run le Map.empty (End (E le))
+    let p' = takeT (sizeFactor*100) p
+    let maximal = p' == takeT (sizeFactor*101) p
+    let value = val p'
+    let stuck = isNothing value
+    let balanced = isBalanced p'
+    --test $ withTimeLimit 10000 $ do
+    test $ do
+      assert (not maximal || stuck || balanced)
+      classify "stuck" stuck
+      classify "balanced" balanced
+      classify "potentially infinite" (not maximal)
+    --forM_ [0..20] $ \n ->
+    --  classify (fromString (printf "larger than %3d" (20*n))) (Gen.exprSize e > 20*n)
+    --forM_ [0..20] $ \n ->
+    --  classify (fromString (printf "longer than %3d" (20*n))) (lenT p' > 20*n)
+
+prop_maxinf_stateless_is_cont =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    let p1 = run le Map.empty (End (E le))
+    let p2 = Cont.run le Map.empty (End (E le))
+    let p1' = takeT (sizeFactor*100) p1
+    let p2' = takeT (sizeFactor*100) p2
+    p1' === Cont.concTrace p2'
+
+prop_direct_cont_abs =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    let p = Cont.run le Map.empty (End (E le))
+    let p' = takeT (sizeFactor*100) p
+    p' === Cont.absTrace (Cont.concTrace p')
+
+prop_stateful_stateless =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    let p1 = Stateful.run le
+    let p2 = Stateless.run le Map.empty (End (E le))
+    let p1' = takeT (sizeFactor*100) p1
+    let p2' = takeT (sizeFactor*100) p2
+    traceLabels p1' === traceLabels p2'
+
+prop_dynamicenv_stateless =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    let p1 = DynamicEnv.run le
+    let p2 = run le Map.empty (End (E le))
+    let p1' = takeT (sizeFactor*100) p1
+    let p2' = takeT (sizeFactor*100) p2
+    traceLabels p1' === traceLabels p2'
+
+prop_envless_stateless =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    let p1 = Envless.run le
+    let p2 = run le Map.empty (End (E le))
+    let p1' = takeT (sizeFactor*100) p1
+    let p2' = takeT (sizeFactor*100) p2
+    traceLabels p1' === traceLabels p2'
+
+prop_stackless_stateless =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    let p1 = Stackless.run le
+    let p2 = run le Map.empty (End (E le))
+    let p1' = takeT (sizeFactor*100) p1
+    let p2' = takeT (sizeFactor*100) p2
+    traceLabels p1' === traceLabels p2'
+
+prop_abs_conc_roundtrip =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    let strace = takeT (sizeFactor*50) $ Stateful.run le
+    let strace' = LessToFull.forward $ LessToFull.backward strace
+    strace === strace'
+    let trace = takeT (sizeFactor*50) $ Stateless.runInit le
+    let trace' = LessToFull.backward $ LessToFull.forward trace
+    trace === trace'
+
+prop_abs_continue_stateful_commutes =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    forM_ [1..20*sizeFactor] $ \n -> do
+      let p1  = takeT n $ Stateless.runInit le
+      let sp1 = LessToFull.forward p1
+      let sp2 = takeT n $ Stateful.run le
+      diff sp1 (Stateful.bisimilarForNSteps 20) sp2
+
+prop_conc_continue_stateless_commutes =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    let le = label e
+    forM_ [1..20*sizeFactor] $ \n -> do
+      let p1  = takeT n $ Stateful.run le
+      let sp1 = LessToFull.backward p1
+      let sp2 = takeT n $ Stateless.runInit le
+      diff sp1 (Stateless.bisimilarForNSteps 20) sp2
+
+prop_stateless_split_prefix =
+  property $ do
+    e <- forAll (Gen.openExpr (Gen.mkEnvWithNVars 2))
+    forM_ [1..20] $ \n -> do
+      let le = label e
+      let p = Stateless.runInit le
+      let pref = takeT n p
+      le' <- case tgt pref of Ret _ -> discard; E le' -> pure le'
+      let suff1 = dropT n p
+      let suff2 = Stateless.run le' (Stateless.materialiseEnv pref) pref
+      let p1 = takeT (sizeFactor*40) suff1
+      let p2 = takeT (sizeFactor*40) suff2
+      let is_pref a b = NE.toList (traceLabels a) `NE.isPrefixOf` traceLabels b
+      diff p2 is_pref p1
+
+eqListBy :: (a -> b -> Bool) -> [a] -> [b] -> Bool
+eqListBy f []     []     = True
+eqListBy f (x:xs) (y:ys) = f x y && eqListBy f xs ys
+eqListBy _ _      _      = False
