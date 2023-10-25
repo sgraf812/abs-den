9a6c8a29d15aa8ad93f162dc50efcdf442f2fb7a
Sebastian Graf <sgraf1337@gmail.com>
2023-10-25 10:15:53 +0200

    Fixing stuff in DmdAnal. Decided to focus on simpler status quo first

--- paper/hs/DmdAnal.hs
+++ paper/hs/DmdAnal.hs
@@ -1,32 +1,51 @@
+{-# LANGUAGE DerivingVia #-}
 module DmdAnal where
 
+import Prelude hiding ((+))
+import Expr
+import Order
 import Interpreter
 import Abstractions
-import Control.Monad.Trans.RWS
+import Numeric.Natural
+import Data.Functor.Identity
+import Control.Monad.Trans.Writer
+import Control.Monad.Trans.Reader
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 
 -- data U = U0 | U1 | Uω -- defined in Abstractions, including ⊔ and +
 type UNonAbs  = U -- Not U0
 type UNonOnce = U -- Not U1
 
-data Demand = U :* SubDemand
+data Demand = U :* SubDemand deriving Eq
 data SubDemand
   = Call UNonAbs SubDemand
   | Data (Tag :-> [Demand])
   | Poly UNonOnce
+  deriving Eq
 
-data NameOrIdx = N Name | I Nat deriving (Eq,Ord)
+instance Lat Demand where
+instance Lat SubDemand where
 
-newtype DmdEnv = DE (Name :-> Demand)
-  deriving (Eq, Lat)
-instance Semigroup DmdEnv where
-  DE φ1 <> DE φ2 = DE (φ1 + φ2)
-instance Monoid DmdEnv where
-  mempty = DE emp
+instance Plussable Demand where
+instance Plussable SubDemand where
 
-newtype DmdT a = DT (RWS SubDemand DmdEnv () a)
-  deriving (Functor,Applicative,Monad)
-type DmdD = DT DmdVal
+absD :: Demand
+absD = U0 :* Poly U0
+
+data NameOrIdx = N Name | I Natural deriving (Eq,Ord)
+
+type DmdEnv = NameOrIdx :-> Demand
+instance Semigroup Demand where
+  d1 <> d2 = d1 + d2
+instance Monoid Demand where
+  mempty = absD
+
+newtype DmdT a = DT (SubDemand -> (a, DmdEnv))
+  deriving (Functor,Applicative,Monad) via ReaderT SubDemand (Writer DmdEnv)
+
+type DmdD = DmdT DmdVal
 data DmdVal
   = DmdFun DmdD
   | DmdCon Tag [DmdD]
@@ -34,17 +53,15 @@ data DmdVal
   | DmdNop -- TODO Don't know what to do here
 
 instance IsTrace DmdT where
-  step (Lookup x) (DT m) = DT $ do
-    sd <- ask
-    tell (ext emp x (U1 :* sd))
-    m
+  step (Lookup x) (DT m) = DT $ \sd ->
+    let (a, φ) = m sd in (a, φ + Map.singleton (N x) (U1 :* sd))
   step _ τ = τ
 
 abstractDmdD :: Name -> DmdD -> DmdD
 abstractDmdD n = go_d
   where
-    go_d (DT m) = DT $ mapWriter (\(v,φ) -> (go_v v, go_env φ)) m
-    go_env (DE φ) = DE (Map.mapKeys go_key φ)
+    go_d (DT m) = DT $ \sd -> let (v,φ) = m sd in (go_v v, go_env φ)
+    go_env φ = Map.mapKeys go_key φ
     go_key (I n)              = I (n+1)
     go_key (N n') | n == n'   = I 0
                   | otherwise = N n'
@@ -53,21 +70,32 @@ abstractDmdD n = go_d
     go_v DmdBot = DmdBot
     go_v DmdNop = DmdNop
 
--- applyDmdD :: Name -> DmdD -> DmdD -> DmdD
--- applyDmdD n d = go_d
---   where
---     go_d (DT m) = DT $ mapWriter (\(v,φ) -> (go_v v, go_env φ)) m
---     go_env (DE φ) = DE (Map.mapKeys go_key φ)
---     go_key (I 0)              = I (n+1)
---     go_key (N n') | n == n'   = I 0
---                   | otherwise = N n'
---     go_v (DmdFun d) = DmdFun (go_d d)
---     go_v (DmdCon k ds) = DmdCon k (map go_d ds)
---     go_v DmdBot = DmdBot
---     go_v DmdNop = DmdNop
+applyDmdD :: Name -> DmdD -> DmdD
+applyDmdD n = go_d
+  where
+    go_d (DT m) = DT $ \sd -> let (v,φ) = m sd in (go_v v, go_env φ)
+    go_env φ = Map.mapKeys go_key φ
+    go_key (I 0)              = N n
+    go_key (I n)              = I (n-1)
+    go_key n'                 = n'
+    go_v (DmdFun d) = DmdFun (go_d d)
+    go_v (DmdCon k ds) = DmdCon k (map go_d ds)
+    go_v DmdBot = DmdBot
+    go_v DmdNop = DmdNop
 
 instance IsValue DmdT DmdVal where
   retStuck = return DmdBot
-  retFun lbl f = return (DmdFun (abstractDmdD lbl (f sentinel))) --TODO: Forward SubDemand, somehow??
-    where
-      sentinel = step (Lookup lbl) (pure DmdNop)
+  retFun lbl f = DT $ \sd ->
+    let sentinel = step (Lookup lbl) (pure DmdNop) in
+    let (_n,sd') = case sd of
+          Call n sd' -> (n, sd')
+          Poly n     -> (n, Poly n)
+          _          -> (Uω, Poly Uω) in
+    let DT body = abstractDmdD lbl (f sentinel) in
+    let (v,φ) = body sd in
+    (DmdFun (body sd), emp)
+  retCon lbl k ds = DT $ do
+    return (DmdCon k ds) -- TODO doesn't look quite right
+  apply (DmdFun (DT body)) arg = DT $ \sd ->
+    let (_,_) = body (Call U1 sd) in
+    applyDmdD body
