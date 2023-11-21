ef9677b3584c3f99704d54ceea4edcf2f655dcdb
Sebastian Graf <sgraf1337@gmail.com>
2023-11-21 10:36:49 +0100

    typeset_math.sh script

--- paper/typeset_math.sh
+++ paper/typeset_math.sh
@@ -1,18 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+set -e
 
 # Read LaTeX code from stdin
 latex_code=$(cat)
-
-# Wrap LaTeX code with necessary tags
-wrapped_latex=$(cat <<EOF
-\\documentclass{article}
-\\begin{document}
-\\[
-$latex_code
-\\]
-\\end{document}
-EOF
-)
 
 # Create a temporary directory
 tmp_dir=$(mktemp -d)
@@ -21,8 +12,26 @@ tmp_dir=$(mktemp -d)
 latex_file="${tmp_dir}/math.tex"
 pdf_file="${tmp_dir}/math.pdf"
 
-# Write wrapped LaTeX code to a file
-echo -e "$wrapped_latex" > "$latex_file"
+cat > "$latex_file" <<EOF
+\\documentclass{standalone}
+\\usepackage[T1]{fontenc} % https://tex.stackexchange.com/a/181119
+\\usepackage[utf8]{inputenc} % https://tex.stackexchange.com/a/181119
+\\usepackage{alphabeta}
+\\newcommand{\\AppIT}{\\textsc{App}_1}
+\\newcommand{\\AppET}{\\textsc{App}_2}
+\\newcommand{\\ValueT}{\\textsc{Val}}
+\\newcommand{\\CaseIT}{\\textsc{Case}_1}
+\\newcommand{\\CaseET}{\\textsc{Case}_2}
+\\newcommand{\\LookupT}{\\textsc{Look}}
+\\newcommand{\\UpdateT}{\\textsc{Upd}}
+\\newcommand{\\BindT}{\\textsc{Bind}}
+\\newcommand{\\LetOT}{\\textsc{Let}_0}
+\\newcommand{\\LetIT}{\\textsc{Let}_1}
+\\begin{document}
+\\fboxsep2pt\\fboxrule0pt\\fbox{\$ $latex_code \$}
+\\end{document}
+EOF
+
 
 cat $latex_file
 
@@ -30,7 +39,7 @@ cat $latex_file
 pdflatex -output-directory="$tmp_dir" "$latex_file"
 
 # Open the PDF file with a PDF viewer
-zathura "$pdf_file"
+zathura --mode=fullscreen "$pdf_file"
 
 # Clean up temporary files
 rm -rf "$tmp_dir"
