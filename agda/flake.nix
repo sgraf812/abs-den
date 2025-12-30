0fa491c68144051fd88a5999a1a00b132b7e0ef3
Sebastian Graf <sgraf1337@gmail.com>
2025-12-30 18:44:41 +0100

    Update flakes

--- agda/flake.nix
+++ agda/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -15,4 +15,3 @@
         };
       });
 }
-
